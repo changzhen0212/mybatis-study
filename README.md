f8a53d39975d0db8f25e311102ac9291088d3baf
changzhen <changzhen@zhen.com>
2022-04-15 16:40:20 +0800

    首次提交

---
+++ README.md
@@ -0,0 +1,18 @@
+# mybatis-3-5.3.x
+
+#### 介绍
+mybatis源码学习
+
+#### 软件架构
+软件架构说明
+
+
+#### 安装教程
+
+1.  直接通过idea导入即可 
+
+#### 使用说明
+
+1.   mybatis-3-5.3 为mybatis源码
+2.   mybatis-spring  为spring整合mybatis源码
+3.   mybatis-test  自定义测试代码
---
+++ mybatis-test/src/main/java/com/example/MybatisMain.java
@@ -0,0 +1,32 @@
+package com.example;
+
+import com.example.entity.User;
+import com.example.mapper.UserMapper;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * @author ChangZhen
+ * @description
+ * @date 2022-3-30
+ */
+public class MybatisMain {
+
+    public static void main(String[] args) throws IOException {
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        SqlSession session = sqlSessionFactory.openSession();
+        User user = (User) session.selectOne("com.example.mapper.UserMapper.selectById", 1);
+        // UserMapper userMapper = session.getMapper(UserMapper.class);
+        // User user = userMapper.selectById(1);
+        System.out.println(user);
+    }
+
+}
